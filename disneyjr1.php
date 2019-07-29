9bd6e03005fce730aec843e073917258e834e9c7
chunglop0781 <chunglop0781@gmail.com>
2019-07-29 19:49:06 +0800

    Update disneyjr1.php

--- disneyjr1.php
+++ disneyjr1.php
@@ -1,1 +1,1 @@
-https://vsd8.mycdn.me/hls/537033509596.m3u8/sig/IuX75NCtjes/expires/1564486085215/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537033509596_high/index.m3u8
+https://vsd4.mycdn.me/hls/535971629788.m3u8/sig/cnCpXTLp4-s/expires/1564487270788/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/535971629788_high/index.m3u8
