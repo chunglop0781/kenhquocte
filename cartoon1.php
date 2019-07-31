d3b17dc33a6bdf09ba1d8525ec092650c1aefa95
chunglop0781 <chunglop0781@gmail.com>
2019-07-31 10:19:39 +0800

    Update cartoon1.php

--- cartoon1.php
+++ cartoon1.php
@@ -1,1 +1,1 @@
-https://vsd4.mycdn.me/hls/537033509596.m3u8/sig/0xFBg-NXTo0/expires/1564625647058/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/
+https://vsd4.mycdn.me/hls/537033509596.m3u8/sig/0xFBg-NXTo0/expires/1564625647058/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537033509596_high/index.m3u8
