0676c88538cc976773ff7d53898986ef2b836c13
chunglop0781 <chunglop0781@gmail.com>
2019-08-01 11:59:00 +0800

    Update cartoon3.php

--- cartoon3.php
+++ cartoon3.php
@@ -1,1 +1,1 @@
-https://vsd8.mycdn.me/hls/537033509596.m3u8/sig/-WwlAxVHvUw/expires/1564713323327/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/
+https://vsd8.mycdn.me/hls/537033509596.m3u8/sig/-WwlAxVHvUw/expires/1564713323327/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537033509596_medium/index.m3u8
