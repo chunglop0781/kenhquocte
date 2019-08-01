e6a0d96fe6bc0c5207f98da456ac58b8cbe54ed9
chunglop0781 <chunglop0781@gmail.com>
2019-08-01 10:41:46 +0800

    Update cartoon4.php

--- cartoon4.php
+++ cartoon4.php
@@ -1,1 +1,1 @@
-https://vsd4.mycdn.me/hls/537033509596.m3u8/sig/0xFBg-NXTo0/expires/1564625647058/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537033509596_lowest/index.m3u8
+https://vsd8.mycdn.me/hls/537033509596.m3u8/sig/-WwlAxVHvUw/expires/1564713323327/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537033509596_lowest/index.m3u8
