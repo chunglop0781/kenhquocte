70b3e5b4534cb814774f0f32feb34084c1cb0d38
chunglop0781 <chunglop0781@gmail.com>
2019-08-01 18:00:13 +0800

    Update mtv1.php

--- mtv1.php
+++ mtv1.php
@@ -1,1 +1,1 @@
-https://vsd0.mycdn.me/hls/535970187996.m3u8/sig/N_HvxSLq2Zo/expires/1564723143341/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/video.m3u8?p
+https://vsd0.mycdn.me/hls/535970187996.m3u8/sig/N_HvxSLq2Zo/expires/1564723143341/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/535970187996_high/index.m3u8
