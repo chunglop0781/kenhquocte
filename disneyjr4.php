deb45f9bc2af400e5a4bceb071d5bb56c0414cd3
chunglop0781 <chunglop0781@gmail.com>
2019-08-01 13:17:16 +0800

    Update disneyjr4.php

--- disneyjr4.php
+++ disneyjr4.php
@@ -1,1 +1,1 @@
-https://vsd0.mycdn.me/hls/535971629788.m3u8/sig/AqVY0mZCg70/expires/1564623468014/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/535971629788_lowest/index.m3u8
+https://vsd0.mycdn.me/hls/535971629788.m3u8/sig/B310prqnADY/expires/1564722622388/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/535971629788_lowest/index.m3u8
